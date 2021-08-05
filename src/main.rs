d993fe70fbd2846f5211deb8adfbe28775db715f
Franklin Delehelle <franklin.delehelle@odena.eu>
2021-08-05 16:39:00 +0200

    CLI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use rand::{thread_rng, seq::SliceRandom};
-use std::{env, io::Write};
-use std::fs::File;
-use std::io::{self, BufRead};
+use clap::{App, Arg, SubCommand};
+use indicatif::ProgressBar;
+use rand::{seq::SliceRandom, thread_rng};
 use rayon::prelude::*;
+use std::fs::File;
+use std::io::Write;
+use std::io::{self, BufRead};
 
 const THRESHOLD: i64 = 2000;
 #[derive(Debug)]
@@ -34,16 +36,31 @@ fn is_ko(snp: &Snp, current: &[Snp]) -> bool {
 }
 
 fn main() {
-    let mut args = env::args();
-    let _ = args.next();
-    let filename = args.next().expect("Please specify input file name");
-    let outfile = args.next().expect("Please specify output file name");
+    let args = App::new("Sample")
+        .arg(
+            Arg::with_name("INPUT_BEAGLE")
+                .help("Set the input beagle file to use")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .short("o")
+                .long("out")
+                .help("Sets the output file")
+                .default_value("out.beagle"),
+        )
+        .get_matches();
 
-    println!("Reading {}; writing result to {}", &filename, &outfile);
+    let filename = args.value_of("INPUT_BEAGLE").unwrap();
+    let outfile = args.value_of("OUTPUT").unwrap();
+
+    println!("Reading {}", &filename);
     let mut snps: Vec<Snp> = io::BufReader::new(File::open(filename).unwrap())
         .lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect::<Vec<_>>()
+        .into_par_iter()
         .map(|l| {
-            let l = l.unwrap();
             let mut s = l.split('\t');
             let name = s.next().expect(&format!("No name found in {}", l));
             let mut ns = name.split('_');
@@ -62,12 +79,18 @@ fn main() {
             }
         })
         .collect();
+
+    println!("Shuffling input");
     snps.shuffle(&mut thread_rng());
 
     let todo = (0.25 * snps.len() as f32 + 1.) as usize;
     let mut done = vec![snps.pop().unwrap()];
+    let bar = ProgressBar::new(snps.len() as u64);
+    bar.set_style(indicatif::ProgressStyle::default_bar().template("[{elapsed_precise}] {bar:80} {pos:>8}/{len:8} ({eta} remaining)"));
 
+    println!("Sampling {} SNPs from {}", todo, snps.len());
     while let Some(new) = snps.pop() {
+        bar.inc(1);
         if !is_ko(&new, &done) {
             done.push(new);
         }
@@ -78,16 +101,32 @@ fn main() {
     }
 
     if done.len() < todo {
-        eprintln!("Not enough valid SNPs; stopping at {} instead of {}", done.len(), todo);
+        eprintln!(
+            "Not enough valid SNPs; stopping at {} instead of {}",
+            done.len(),
+            todo
+        );
     }
     done.sort_by_key(|snp| (snp.scaffold.to_owned(), snp.pos));
 
+    println!("Writing result to {}", &outfile);
     let mut out = File::create(&outfile).expect(&format!("Can't create `{}`", &outfile));
     for snp in done {
-        out.write_all(format!("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n",
-                              snp.name, snp.a_ref, snp.a_alt,
-                              snp.p11, snp.p12, snp.p13,
-                              snp.p21, snp.p22, snp.p23,
-        ).as_bytes()).unwrap();
+        out.write_all(
+            format!(
+                "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n",
+                snp.name,
+                snp.a_ref,
+                snp.a_alt,
+                snp.p11,
+                snp.p12,
+                snp.p13,
+                snp.p21,
+                snp.p22,
+                snp.p23,
+            )
+                .as_bytes(),
+        )
+            .unwrap();
     }
 }
