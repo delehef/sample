a4b73bc81a94fb2da624e61da02fe94b7c1b6367
Franklin Delehelle <franklin.delehelle@odena.eu>
2021-08-14 13:34:21 +0200

    Add a threshold option

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg, SubCommand};
+use clap::*;
 use indicatif::ProgressBar;
 use rand::{seq::SliceRandom, thread_rng};
 use rayon::prelude::*;
@@ -6,7 +6,6 @@ use std::fs::File;
 use std::io::Write;
 use std::io::{self, BufRead};
 
-const THRESHOLD: i64 = 2000;
 #[derive(Debug)]
 struct Snp {
     scaffold: String,
@@ -15,24 +14,19 @@ struct Snp {
     name: String,
     a_ref: u8,
     a_alt: u8,
-    p11: f32,
-    p12: f32,
-    p13: f32,
-    p21: f32,
-    p22: f32,
-    p23: f32,
+    ps: Vec<String>,
 }
 
-fn incompatible(snp1: &Snp, snp2: &Snp) -> bool {
+fn incompatible(snp1: &Snp, snp2: &Snp, threshold: i64) -> bool {
     if snp1.scaffold != snp2.scaffold {
         false
     } else {
-        (snp1.pos - snp2.pos).abs() < THRESHOLD
+        (snp1.pos - snp2.pos).abs() < threshold
     }
 }
 
-fn is_ko(snp: &Snp, current: &[Snp]) -> bool {
-    current.par_iter().any(|old| incompatible(snp, old))
+fn is_ko(snp: &Snp, current: &[Snp], threshold: i64) -> bool {
+    current.par_iter().any(|old| incompatible(snp, old, threshold))
 }
 
 fn main() {
@@ -49,10 +43,18 @@ fn main() {
                 .help("Sets the output file")
                 .default_value("out.beagle"),
         )
+        .arg(
+            Arg::with_name("THRESHOLD")
+                .short("t")
+                .long("threshold")
+                .help("The minimum distance (in bp) between two SNPs to sample")
+                .default_value("2000"),
+        )
         .get_matches();
 
     let filename = args.value_of("INPUT_BEAGLE").unwrap();
     let outfile = args.value_of("OUTPUT").unwrap();
+    let threshold = value_t!(args, "THRESHOLD", i64).unwrap();
 
     println!("Reading {}", &filename);
     let mut snps: Vec<Snp> = io::BufReader::new(File::open(filename).unwrap())
@@ -70,12 +72,7 @@ fn main() {
                 name: name.to_owned(),
                 a_ref: s.next().unwrap().parse().unwrap(),
                 a_alt: s.next().unwrap().parse().unwrap(),
-                p11: s.next().unwrap().parse().unwrap(),
-                p12: s.next().unwrap().parse().unwrap(),
-                p13: s.next().unwrap().parse().unwrap(),
-                p21: s.next().unwrap().parse().unwrap(),
-                p22: s.next().unwrap().parse().unwrap(),
-                p23: s.next().unwrap().parse().unwrap(),
+                ps: s.map(str::to_owned).collect(),
             }
         })
         .collect();
@@ -86,12 +83,15 @@ fn main() {
     let todo = (0.25 * snps.len() as f32 + 1.) as usize;
     let mut done = vec![snps.pop().unwrap()];
     let bar = ProgressBar::new(snps.len() as u64);
-    bar.set_style(indicatif::ProgressStyle::default_bar().template("[{elapsed_precise}] {bar:80} {pos:>8}/{len:8} ({eta} remaining)"));
+    bar.set_style(
+        indicatif::ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {bar:80} {pos:>8}/{len:8} ({eta} remaining)"),
+    );
 
-    println!("Sampling {} SNPs from {}", todo, snps.len());
+    println!("Sampling {} SNPs >{}bp apart from {}", todo, threshold, snps.len());
     while let Some(new) = snps.pop() {
         bar.inc(1);
-        if !is_ko(&new, &done) {
+        if !is_ko(&new, &done, threshold) {
             done.push(new);
         }
 
@@ -114,19 +114,14 @@ fn main() {
     for snp in done {
         out.write_all(
             format!(
-                "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n",
+                "{}\t{}\t{}\t{}\n",
                 snp.name,
                 snp.a_ref,
                 snp.a_alt,
-                snp.p11,
-                snp.p12,
-                snp.p13,
-                snp.p21,
-                snp.p22,
-                snp.p23,
+                snp.ps.join("\t"),
             )
-                .as_bytes(),
+            .as_bytes(),
         )
-            .unwrap();
+        .unwrap();
     }
 }
