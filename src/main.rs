0e067fcd2992134ee52c2fbb97f094f333a8c5bb
Franklin Delehelle <franklin.delehelle@odena.eu>
2021-12-28 17:02:03 +0100

    Can set the quotity of SNPs to sample

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn is_ko(snp: &Snp, current: &[Snp], threshold: i64) -> bool {
     current.par_iter().any(|old| incompatible(snp, old, threshold))
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = App::new("Sample")
         .arg(
             Arg::with_name("INPUT_BEAGLE")
@@ -51,6 +51,7 @@ fn main() {
                 .short("o")
                 .long("out")
                 .help("Sets the output file")
+                .takes_value(true)
                 .default_value("out.snps"),
         )
         .arg(
@@ -65,12 +66,21 @@ fn main() {
                 .short("t")
                 .long("threshold")
                 .help("The minimum distance (in bp) between two SNPs to sample")
+                .takes_value(true)
                 .default_value("2000"),
+        )
+        .arg(
+            Arg::with_name("QUANTITY")
+                .short("Q")
+                .long("quantity")
+                .help("The maximum proportion of SNPs to sample. 1.0 means sample all, 0.5 means sample half.")
+                .takes_value(true)
+                .default_value("1")
         )
         .get_matches();
 
     let mut snps: Vec<Snp> = if args.is_present("INPUT_BEAGLE") {
-        let filename = args.value_of("INPUT_BEAGLE").unwrap();
+        let filename = value_t!(args, "INPUT_BEAGLE", String)?;
         println!("Reading {}", &filename);
         io::BufReader::new(File::open(filename).unwrap())
             .lines()
@@ -92,9 +102,9 @@ fn main() {
             })
             .collect()
     } else if args.is_present("INPUT_SIMPLE"){
-        let filename = args.value_of("INPUT_SIMPLE").unwrap();
+        let filename = value_t!(args, "INPUT_SIMPLE", String)?;
         println!("Reading {}", &filename);
-        io::BufReader::new(File::open(value_t!(args, "INPUT_SIMPLE", String).unwrap()).unwrap())
+        io::BufReader::new(File::open(&filename).unwrap())
             .lines()
             .map(|l| l.expect("Could not read line"))
             .filter(|l| !l.is_empty())
@@ -115,21 +125,23 @@ fn main() {
     } else {
         panic!("Please set an input file with --in-beagle or --in-simple")
     };
-    let outfile = args.value_of("OUTPUT").unwrap();
-    let threshold = value_t!(args, "THRESHOLD", i64).unwrap();
+    let outfile = value_t!(args, "OUTPUT", String)?;
+    let threshold = value_t!(args, "THRESHOLD", i64)?;
 
 
     println!("Shuffling input");
     snps.shuffle(&mut thread_rng());
 
-    let todo = (0.25 * snps.len() as f32 + 1.) as usize;
-    let mut done = vec![snps.pop().unwrap()];
     let bar = ProgressBar::new(snps.len() as u64);
+    let mut done = vec![snps.pop().unwrap()];
     bar.set_style(
         indicatif::ProgressStyle::default_bar()
             .template("[{elapsed_precise}] {bar:80} {pos:>8}/{len:8} ({eta} remaining)"),
     );
 
+
+    let quantity = value_t!(args, "QUANTITY", f32)?;
+    let todo = (quantity * snps.len() as f32 + 1.) as usize;
     println!("Sampling {} SNPs >{}bp apart from {}", todo, threshold, snps.len());
     while let Some(new) = snps.pop() {
         bar.inc(1);
@@ -153,11 +165,12 @@ fn main() {
 
     println!("Writing result to {}", &outfile);
     let mut out = File::create(&outfile).expect(&format!("Can't create `{}`", &outfile));
-    let format = value_t!(args, "OUTPUT_FORMAT", String).unwrap();
+    let format = value_t!(args, "OUTPUT_FORMAT", String)?;
     for snp in done {
         out.write_all(format_snp_out(&snp, &format).as_bytes())
         .unwrap();
     }
+    Ok(())
 }
 
 fn format_snp_out(snp: &Snp, format: &str) -> String {
